fbb5efb25da2a0a015979e1f818b85fc454685bc
shashank <shivashashank32@gmail.com>
2026-02-16 01:33:41 +0530

    added practice package of arrays of new branch

---
+++ src/com/java_dsa/practice_programs/arrays/FindMaxElement.java
@@ -0,0 +1,19 @@
+package com.java_dsa.practice_programs.arrays;
+
+public class FindMaxElement {
+    public static void main(String[] args) {
+        int[] numbers = {10, 45, 23, 89, 12, 67};
+        int max = findMax(numbers);
+        System.out.println("Maximum element: " + max);
+    }
+    
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for(int num : arr) {
+            if(num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+}
