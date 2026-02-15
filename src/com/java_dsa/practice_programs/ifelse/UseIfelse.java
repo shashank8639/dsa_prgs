527f3a15410ac8f434adc5daa14eaa34b9fcb6bb
shashank <shivashashank32@gmail.com>
2026-02-16 02:48:57 +0530

    added ifelse package with demo program for prac

---
+++ src/com/java_dsa/practice_programs/ifelse/UseIfelse.java
@@ -0,0 +1,14 @@
+package com.java_dsa.practice_programs.ifelse;
+
+public class UseIfelse {
+
+	public static void main(String[] args) {
+
+		int age = 21;
+		if(age >= 21)
+			System.out.println("Eligible to take alchohol");
+		else
+			System.out.println("Not eligible can take when ur 21 or above");
+	}
+
+}
